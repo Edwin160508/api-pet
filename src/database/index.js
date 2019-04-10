3b7d9f55a6e6da92d2afb1bc70a2d8c2e5c9f277
Edwin Lima <edwin.limaa@gmail.com>
2019-04-10 09:17:54 -0300

    Incluindo option useNewUrl true. no database

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/apipet', {});
+mongoose.connect('mongodb://localhost/apipet', { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
